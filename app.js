9997e4204247e463de23252b7ebbd1c22921aac8
kevinesc21 <00116120@uca.edu.sv>
2022-07-12 23:40:11 -0600

    Labo 3 sin tarea

---
+++ app.js
@@ -0,0 +1,31 @@
+require("./database/Config")
+var userRouter = require("./routers/user");
+var wordRouter = require("./routers/word");
+var authRouter = require("./routers/auth")
+var passport = require("passport");
+require("./auth/auth");
+
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var helmet = require("helmet");
+
+var app = express();
+
+
+app.use(logger("dev"));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/users", userRouter);
+app.use(authRouter);
+app.use("/words", wordRouter);
+
+//middlewares
+passport.authenticate("jwt", { session: false })
+
+module.exports = app;
